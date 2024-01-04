a1e48856e0dc04c1cbc9fa19b5f97c5a016b1a89
benammarrouaa <benammar24roua@gmail.com>
2024-01-04 10:45:13 +0100

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import MainPage from './components/MainPage';
+import './App.css';
+
+const App: React.FC = () => {
+    return (
+    <div className="App">
+     <MainPage/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/MainPage.tsx
@@ -0,0 +1,12 @@
+// Use the interface to type the props parameter
+const MainPage: React.FC = () => {
+  return (
+    <div>
+      <div>Hello word</div>
+			<div></div>
+      <div></div>
+    </div>
+  );
+};
+
+export default MainPage;
